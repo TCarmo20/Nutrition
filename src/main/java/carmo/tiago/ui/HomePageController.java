49c650786b009810f2db19dbd9877842df83d589
TCarmo20 <tcarmo20@gmail.com>
2017-03-03 16:57:40 +0000

    menu e table na home

--- src/main/java/carmo/tiago/ui/HomePageController.java
+++ src/main/java/carmo/tiago/ui/HomePageController.java
@@ -11,8 +11,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 
 /**
  * Java FX FXML Controller.
@@ -33,9 +33,8 @@ public class HomePageController implements Initializable {
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		try {
-			VBox vbox = FXMLLoader.load(getClass().getResource("/DrawerContent.fxml"));
-			drawer.setSidePane(vbox);
-			drawer.toFront();
+			AnchorPane anchor = FXMLLoader.load(getClass().getResource("/DrawerContent.fxml"));
+			drawer.setSidePane(anchor);
 			HamburgerBackArrowBasicTransition hamTran = new HamburgerBackArrowBasicTransition(hamburger);
 			hamTran.setRate(-1);
 			hamburger.addEventHandler(MouseEvent.MOUSE_PRESSED,(e)->{
